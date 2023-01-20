df436b12f85e10fcf21969a5da11b53c631fb180
Lars Kolbowski <lars.kolbowski@tu-berlin.de>
2023-01-20 17:11:38 +0100

    add score names dict

---
+++ score_names.py
@@ -0,0 +1,10 @@
+score_names = {
+    # 'Kojak': 'E_value',
+    'Kojak': 'K_score',
+    # 'Kojak': 'iProbability',
+    'MangoCometXlinkProphet': 'probability',
+    'pLink2': '1 - Score',
+    'ProteinProspector': 'SVM.score',
+    'xiSEARCH': 'Score',
+    'XlinkX': 'XlinkX Score',
+}
