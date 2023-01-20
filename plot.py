1437154422bd3526607b1575db89d0e6f7313f2e
Lars Kolbowski <lars.kolbowski@tu-berlin.de>
2023-01-20 17:32:24 +0100

    add process and plot scripts

---
+++ plot.py
@@ -0,0 +1,54 @@
+from plots_and_functions import plot_distribution
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+
+# read in the results
+df = pd.read_csv(os.path.join('search_results', 'processed_results.csv'))
+
+# single plots
+# Kojak
+# score = 'E_value'
+score = 'K_score'
+# score = 'iProbability'
+kojak_df = df[df['search_engine'] == 'Kojak']
+plot_distribution(df=kojak_df, x=score, bins=50)
+plt.show()
+plot_distribution(df=kojak_df, x=score, bins=50, ylim_top=100)
+plt.show()
+
+# Mango Comet XlinkProphet
+mango_df = df[df['search_engine'] == 'MangoCometXlinkProphet']
+plot_distribution(df=mango_df, x='score', bins=50)
+plt.show()
+plot_distribution(df=mango_df, x='score', bins=50, ylim_top=300)
+plt.show()
+
+# pLink2
+plink2_df = df[df['search_engine'] == 'pLink2']
+bins = np.arange(plink2_df['score'].min(), plink2_df['score'].max(), 0.025)
+plot_distribution(df=plink2_df, x='score', bins=bins)
+plt.show()
+plot_distribution(df=plink2_df, x='score', bins=bins, ylim_top=120)
+plt.show()
+
+# proteinProspector
+pp_df = df[df['search_engine'] == 'ProteinProspector']
+plot_distribution(df=pp_df, x='score', bins=50)
+plt.show()
+plot_distribution(df=pp_df, x='score', bins=50, ylim_top=300)
+plt.show()
+
+# xiSEARCH
+xisearch_df = df[df['search_engine'] == 'xiSEARCH']
+plot_distribution(df=xisearch_df, x='score', bins=50)
+plt.show()
+plot_distribution(df=xisearch_df, x='score', bins=50, ylim_top=50)
+plt.show()
+
+# XlinkX
+xlinkx_df = df[df['search_engine'] == 'XlinkX']
+ax = plot_distribution(df=xlinkx_df, x='score', bins=50)
+plt.show()
+
---
+++ process.py
@@ -0,0 +1,50 @@
+from process_results import *
+from plots_and_functions import fasta_to_dict
+
+import os
+import pandas as pd
+
+
+# read in E. coli fasta file
+ecoli_fasta = fasta_to_dict('database/uniprot-k12-filtered-proteome_UP000000625.fasta')
+# read in uniprot IDs of E. coli as list
+ecoli_proteins = list(ecoli_fasta.keys())
+
+#
+# process the results
+#
+# Kojak
+kojak_result_file = os.path.join('search_results', 'kojak', 'Kojak_CSMs_NoLoopLinks.tsv')
+kojak_df = process_kojak(kojak_result_file, ecoli_proteins)
+
+# Mango Comet XlinkProphet
+mango_result_file = os.path.join('search_results', 'mango_comet_xlinkprophet',
+                                 'iprophet-withdecoys-xl-filtered_2pct.xlsx')
+mango_df = process_mango_comet_xlinkprophet(mango_result_file, ecoli_proteins)
+
+# pLink2
+plink2_result_file = os.path.join('search_results', 'plink2', 'DSSO_HumanDB_plinkFDR_xifdr.csv')
+pllink2_unfiltered_result_file = os.path.join('search_results', 'plink2',
+                                              'DSSO_HumanDB_unfiltered_xifdr.csv')
+plink2_df = process_plink2(plink2_result_file, pllink2_unfiltered_result_file, ecoli_proteins)
+
+# proteinProspector
+pp_result_file = os.path.join('search_results', 'proteinprospector', 'ec1_dsso_CSM_2p_inter.csv')
+pp_df = process_proteinprospector(pp_result_file, ecoli_proteins)
+
+# xiSEARCH
+xisearch_result_file = os.path.join(
+        'search_results', 'xisearch',
+        'xiSEARCH_CSM_FDR_proteome+human3_linMod_filtered_0_PSM_xiFDR1.3.36.csv')
+xisearch_df = process_xisearch(xisearch_result_file, ecoli_proteins)
+
+# XlinkX
+xlinkx_result_file = os.path.join('search_results', 'xlinkx', '2p_all_files_plus3_CSMs.txt')
+xlinkx_decoy_file = os.path.join('search_results', 'xlinkx', '2p_all_files_plus3_DecoyCSMs.txt')
+xlinkx_df = process_xlinkx(xlinkx_result_file, xlinkx_decoy_file, ecoli_proteins)
+
+# concatenate all dataframes
+df = pd.concat([kojak_df, mango_df, plink2_df, pp_df, xisearch_df, xlinkx_df], ignore_index=True)
+
+# save to csv
+df.to_csv(os.path.join('search_results', 'processed_results.csv'), index=False)
